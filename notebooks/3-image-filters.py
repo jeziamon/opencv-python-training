75b3ea8e8cc0ad1f52e31904329e5ceb3a9968b6
Amon, Jezimeel D <jezimeel.d.amon@accenture.com>
2018-06-14 17:21:14 +0800

    chore: update notebook3

--- notebooks/3-image-filters.py
+++ notebooks/3-image-filters.py
@@ -6,7 +6,9 @@ def apply_invert(frame):
  
 def apply_sepia(frame, intensity=0.5):
     blue, green, red = 20, 66, 112
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    frame = apply_alpha_convert(frame)
+    
     frame_height, frame_width, frame_channel = frame.shape
     
     sepia_bgra = (blue, green, red, 1);
@@ -18,7 +20,34 @@ def apply_sepia(frame, intensity=0.5):
     
     return frame
     
- 
+def apply_alpha_convert(frame):
+    try:
+        frame.shape[3]
+    except IndexError:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    return frame
+
+def apply_portrait_mode(frame): 
+    frame = apply_alpha_convert(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(gray, 120,255, cv2.THRESH_BINARY)
+    
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGRA)
+    blurred = cv2.GaussianBlur(frame, (21,21), 0)
+    
+    blended = apply_blend(frame, blurred, mask)
+    frame = cv2.cvtColor(blended, cv2.COLOR_BGRA2BGR)
+    
+    cv2.imshow('mask', mask)
+    cv2.imshow('blurred', blurred)
+    
+    return frame
+
+def apply_blend(frame_1, frame_2, mask):
+    alpha = mask /255.0
+    blended = cv2.convertScaleAbs(frame_1 * (1 - alpha) + frame_2 * alpha)
+    return blended
+
 def apply_green(frame, intensity=0.5):
     blue, green, red = 0, 255, 255
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
@@ -35,6 +64,7 @@ def apply_green(frame, intensity=0.5):
     
     
 cap = cv2.VideoCapture(0)
+
  
 while True:
     _, frame = cap.read()
@@ -42,11 +72,14 @@ while True:
     invert = apply_invert(frame)
     sepia = apply_sepia(frame)
     green = apply_green(frame)
+    portrait = apply_portrait_mode(frame)
     
+    apply_portrait_mode(frame)
     cv2.imshow('frame', frame)
     cv2.imshow('ivnert', invert)
-    cv2.imshow('sepia', sepia)
-    cv2.imshow('green', green)
+    #cv2.imshow('sepia', sepia)
+    #cv2.imshow('green', green)
+    cv2.imshow('portrait', portrait)
     
     k = cv2.waitKey(1)
     
